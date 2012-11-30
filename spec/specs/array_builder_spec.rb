9c07009875212f2176aaef51d6e8773593363157
mo k <mo@mokhan.ca>
2012-11-29 17:51:44 -0700

    change up spec

--- spec/specs/array_builder_spec.rb
+++ spec/specs/array_builder_spec.rb
@@ -9,7 +9,7 @@ module RArrays
     context "when configuring an array to be readable" do
       before(:each) do
         sut.readable
-        step_factory.stub(:readable).and_return(readable_step)
+        step_factory.stub(:create_for).with(:readable).and_return(readable_step)
       end
       it "should add a readable step to the recipe" do
         steps.include?(readable_step).should be_true
@@ -19,7 +19,7 @@ module RArrays
     context "when configuring an array to be writeable" do
       let(:writeable_step) { fake }
       before(:each) do
-        step_factory.stub(:writeable).and_return(writeable_step)
+        step_factory.stub(:create_for).with(:writeable).and_return(writeable_step)
       end
       before(:each) do
         sut.writeable
@@ -31,7 +31,7 @@ module RArrays
     context 'when adding an aliased mutator method' do
       let(:mutator_step) { fake }
       before(:each) do
-        step_factory.stub(:mutator).with(:add_item).and_return(mutator_step)
+        step_factory.stub(:create_for).with(:mutator).with(:add_item).and_return(mutator_step)
       end
       before(:each) do
         sut.mutator :add_item
