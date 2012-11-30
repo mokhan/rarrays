2cadfa2dc7d53ad7895ca1c2dd05c3a50b20e11c
mo k <mo@mokhan.ca>
2012-11-29 17:20:32 -0700

    ooops forgot the because block.

--- spec/specs/array_builder_spec.rb
+++ spec/specs/array_builder_spec.rb
@@ -4,6 +4,15 @@ module RArrays
   describe ArrayBuilder do
     let(:sut) { ArrayBuilder.new(:blah) }
     context "when configuring an array to be readable" do
+      before(:each) do
+        class Item
+          def initializer
+            rarray :blah do |c|
+              c.readable
+            end
+          end
+        end
+      end
       it "should add a readable step to the recipe" do
         sut.steps.include?(:readable).should be_true
       end
