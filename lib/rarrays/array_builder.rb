79587c091b2b67fe0edf0c0766b585935f6a7075
mo k <mo@mokhan.ca>
2012-11-29 17:16:14 -0700

    change design direction as per sensei's instructions.

--- lib/rarrays/array_builder.rb
+++ lib/rarrays/array_builder.rb
@@ -1,9 +1,10 @@
 module RArrays
   class ArrayBuilder
+    attr_reader :steps
     def initialize(name)
-
+      @steps = []
     end
-    def apply_to(target, builder = nil)
+    def apply_to(target)
       
     end
   end
--- spec/specs/array_builder_spec.rb
+++ spec/specs/array_builder_spec.rb
@@ -2,18 +2,10 @@ require 'spec_helper'
 
 module RArrays
   describe ArrayBuilder do
-    let(:sut) { ArrayBuilder.new(attribute) }
-    let(:attribute) { :justin }
-    let(:attribute_builder) { fake }
-    let(:target) { Object.new }
-
-    context "when applied against a target instance" do
-      before(:each) do
-        sut.apply_to(target, attribute_builder)
-      end
-
-      it "should invoke the attribute builder against the target with a default initializer for the array" do
-        attribute_builder.should have_received(:attach_attribute, { :attribute => :address, :default_value => [], :target => target })
+    let(:sut) { ArrayBuilder.new(:blah) }
+    context "when configuring an array to be readable" do
+      it "should add a readable step to the recipe" do
+        sut.steps.include?(:readable).should be_true
       end
     end
   end
