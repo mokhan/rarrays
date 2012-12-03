e190f8db98ddaa9307cbd82daf4689992bd89357
mo k <mo@mokhan.ca>
2012-12-03 08:18:39 -0700

    implement apply_to

--- lib/rarrays/array_builder.rb
+++ lib/rarrays/array_builder.rb
@@ -1,13 +1,16 @@
 module RArrays
   class ArrayBuilder
     attr_reader :steps
-    def initialize(name, configuration = {})
+    def initialize(attribute, configuration = {})
+      @attribute = attribute
       @steps = configuration[:steps]
       @step_factory = configuration[:step_factory]
       @steps << @step_factory.create_for(:init_step)
     end
     def apply_to(target)
-
+      @steps.each do |step|
+        step.run_against({ :target => target, :attribute => @attribute})
+      end
     end
 
     def readable
