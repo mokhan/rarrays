98e33a2a1933c2f8ec29c9258cb39cad0447d480
mo k <mo@mokhan.ca>
2012-12-03 08:55:31 -0700

    put step factory in rarrays module.

--- lib/rarrays/step_factory.rb
+++ lib/rarrays/step_factory.rb
@@ -1,10 +1,12 @@
-class StepFactory
-  def initialize(registry)
-    @registry = registry
-  end
-  def create_for(step)
-    item = Object.new
-    item.extend(@registry.find_for(step))
-    item
+module RArrays
+  class StepFactory
+    def initialize(registry)
+      @registry = registry
+    end
+    def create_for(step)
+      item = Object.new
+      item.extend(@registry.find_for(step))
+      item
+    end
   end
 end
--- spec/specs/step_factory_spec.rb
+++ spec/specs/step_factory_spec.rb
@@ -1,28 +1,30 @@
 require 'spec_helper'
 
-describe StepFactory do
-  let(:registry) { fake }
-  let(:sut) { StepFactory.new(registry) }
+module RArrays
+  describe StepFactory do
+    let(:registry) { fake }
+    let(:sut) { StepFactory.new(registry) }
 
-  context "when creating a step for a specific symbol" do
-    let(:behaviour) do
-      Module.new do
-        define_method :run_against do
-          p 'running'
+    context "when creating a step for a specific symbol" do
+      let(:behaviour) do
+        Module.new do
+          define_method :run_against do
+            p 'running'
+          end
         end
       end
-    end
 
-    before(:each) do
-      registry.stub(:find_for).with(:test_step).and_return(behaviour)
-    end
+      before(:each) do
+        registry.stub(:find_for).with(:test_step).and_return(behaviour)
+      end
 
-    let(:result) do
-      sut.create_for(:test_step)
-    end
+      let(:result) do
+        sut.create_for(:test_step)
+      end
 
-    it "should return an object with the proper behaviour mixed in" do
-      result.respond_to?(:run_against).should be_true
+      it "should return an object with the proper behaviour mixed in" do
+        result.respond_to?(:run_against).should be_true
+      end
     end
   end
 end
